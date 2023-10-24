f31a6e4c5e7a9bb88d7b2697334eff070415c187
bborkowsp <pawel.borkowski0704@gmail.com>
2023-10-24 18:23:03 +0200

    Initial commit

---
+++ src/main/java/com/edm/edmsystem/ElectronicDocumentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.edm.edmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicDocumentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicDocumentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edm/edmsystem/ElectronicDocumentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edm.edmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicDocumentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
