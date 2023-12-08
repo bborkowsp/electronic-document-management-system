d606f03e7d912245c9334ac0809a45d57fea9af6
bborkowsp <pawel.borkowski0704@gmail.com>
2023-12-08 23:10:16 +0100

    almost done document table3

--- emdsystem-frontend/src/app/document/components/document-list/document-list.component.ts
+++ emdsystem-frontend/src/app/document/components/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {Observable, tap} from "rxjs";
 import {MatPaginator} from '@angular/material/paginator';
 
@@ -7,47 +7,39 @@ import {Document} from "../../document";
 
 
 @Component({
-  selector: 'app-document-list',
-  templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.scss'],
+    selector: 'app-document-list',
+    templateUrl: './document-list.component.html',
+    styleUrls: ['./document-list.component.scss'],
 })
-export class DocumentListComponent implements OnInit {
-  readonly displayedColumns: string[] = [
-    'id',
-    // 'documentType',
-    // 'documentNumber',
-    // 'receiptDate',
-    // 'saleDate',
-    // 'issueDate',
-    // 'receiptDate',
-    // 'grossAmount',
-    // 'netAmount',
-    // 'currency',
-    // 'supplierCompany',
-    // 'recipientCompany',
-  ];
-  documents$: Observable<Document[]>;
-  dataLength: number = 0;
-  @ViewChild(MatPaginator) readonly paginator!: MatPaginator;
+export class DocumentListComponent {
+    readonly displayedColumns: string[] = [
+        'id',
+        'documentType',
+        'documentNumber',
+        'receiptDate',
+        'saleDate',
+        'issueDate',
+        'receiptDate',
+        'grossAmount',
+        'netAmount',
+        'currency',
+        'supplierCompany',
+        'recipientCompany',
+    ];
+    documents$: Observable<Document[]>;
+    dataLength: number = 0;
+    @ViewChild(MatPaginator) readonly paginator!: MatPaginator;
 
-  constructor(private readonly documentService: DocumentService) {
-    console.log('Before getData');
+    constructor(private readonly documentService: DocumentService) {
+        this.documents$ = this.getData();
+    }
 
-    this.documents$ = this.getData();
-  }
-
-  ngOnInit() {
-    this.getData();
-  }
-
-  getData(): Observable<Document[]> {
-    console.log('im here');
-
-    return this.documentService.getAllDocuments().pipe(
-      tap({
-        next: (documents) => (this.dataLength = documents.length),
-        error: (err) => console.log(err),
-      }),
-    );
-  }
+    getData(): Observable<Document[]> {
+        return this.documentService.getAllDocuments().pipe(
+            tap({
+                next: (documents) => (this.dataLength = documents.length),
+                error: (err) => console.log(err),
+            }),
+        );
+    }
 }
